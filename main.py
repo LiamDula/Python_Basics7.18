6859aaeac41f248896caa9d7733e0bf5898abc0e
Liam Dulaney <85806676+LiamDula@users.noreply.github.com>
2022-07-18 17:07:09 +0000

    List

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-name = "Liam"
-print("Hello "+ name)
+# name = "Liam"
+# print("Hello "+ name)
+# fruits = ["apple", "orange", "cherry"]
+# print(fruits)
+# fruits.append("banana")
+# print(fruits)
+# print(fruits[0])
+# print(fruits[3])
+# print(fruits[-1])
+# fruits.insert(1, "pineapple")
+# print(fruits)
+# print(len(fruits))
+# for i in range(len(fruits)):
+#   print(fruits[i])
+
+# for value in fruits:
+#   print(value)
+
+# t = (1,2,3,4,5)
+# print(len(t))
